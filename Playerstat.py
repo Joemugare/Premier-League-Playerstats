63733a503bb68f5c797209cc9b86510e5f4fa28e
Joemugare <134427671+Joemugare@users.noreply.github.com>
2024-03-16 11:33:49 +0300

    Add files via upload

--- Playerstat.py
+++ Playerstat.py
@@ -70,57 +70,59 @@ else:
 
     # Displaying player info and premierleague.jpg on the same line
     col1, col2 = st.columns([1, 3])
+
     with col1:
         st.subheader("Player Info:")
         player_info = df_selection[["Player", "Nation", "Pos", "Squad", "Born"]].iloc[0]
         st.write(player_info)
+
     with col2:
-        premier_league_img_path = os.path.join(current_dir, 'imgs', 'premierleague.jpg')
-        st.image(premier_league_img_path, use_column_width=False, width=450)  # Increase the width here
+        lion_gif_path = os.path.join(current_dir, 'imgs', 'lion2gif.gif')
+        st.image(lion_gif_path, use_column_width=False, width=500)  # Adjust the width as per your requirement
 
-    # Displaying the selected stats inside columns
-    st.subheader("Player Statistics:")
-    left_column, middle_column, right_column = st.columns(3)
-    with left_column:
-         st.subheader("Goals")
-         st.subheader(f"{goals}")
-    with middle_column:
-         st.subheader("Assists")
-         st.subheader(f"{assists}")
-    with right_column:
-         st.subheader("Expected goals")
-         st.subheader(f"{expectedgoals}")
+        # Displaying the selected stats inside columns
+        st.subheader("Player Statistics:")
+        left_column, middle_column, right_column = st.columns(3)
+        with left_column:
+             st.subheader("Goals")
+             st.subheader(f"{goals}")
+        with middle_column:
+             st.subheader("Assists")
+             st.subheader(f"{assists}")
+        with right_column:
+             st.subheader("Expected goals")
+             st.subheader(f"{expectedgoals}")
 
-    # Custom CSS for glowing effect
-    st.markdown(
-        """
-        <style>
-        .cover-glow {
-            width: 100%;
-            height: auto;
-            padding: 3px;
-            box-shadow: 
-                0 0 5px #330000,
-                0 0 10px #660000,
-                0 0 15px #990000,
-                0 0 20px #CC0000,
-                0 0 25px #FF0000,
-                0 0 30px #FF3333,
-                0 0 35px #FF6666;
-            position: relative;
-            z-index: -1;
-            border-radius: 30px;  /* Rounded corners */
-        }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
+        # Custom CSS for glowing effect
+        st.markdown(
+            """
+            <style>
+            .cover-glow {
+                width: 100%;
+                height: auto;
+                padding: 3px;
+                box-shadow: 
+                    0 0 5px #330000,
+                    0 0 10px #660000,
+                    0 0 15px #990000,
+                    0 0 20px #CC0000,
+                    0 0 25px #FF0000,
+                    0 0 30px #FF3333,
+                    0 0 35px #FF6666;
+                position: relative;
+                z-index: -1;
+                border-radius: 30px;  /* Rounded corners */
+            }
+            </style>
+            """,
+            unsafe_allow_html=True,
+        )
 
-    # Load and display sidebar image with glowing effect
-    img_path = os.path.join(current_dir, 'imgs', 'premier.png')
-    img_base64 = img_to_base64(img_path)
-    st.sidebar.markdown(
-        f'<img src="data:image/png;base64,{img_base64}" class="cover-glow">',
-        unsafe_allow_html=True,
-    )
-    st.sidebar.markdown("---")
+        # Load and display sidebar image with glowing effect
+        img_path = os.path.join(current_dir, 'imgs', 'premier.png')
+        img_base64 = img_to_base64(img_path)
+        st.sidebar.markdown(
+            f'<img src="data:image/png;base64,{img_base64}" class="cover-glow">',
+            unsafe_allow_html=True,
+        )
+        st.sidebar.markdown("---")
